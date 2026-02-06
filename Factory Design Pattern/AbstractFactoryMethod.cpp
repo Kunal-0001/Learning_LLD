6159762b61467474bceab10ce3d2aacaa23fcb1e
Kunal Singh Rana <kunalsinghrana4@gmail.com>
2026-02-06 21:54:35 +0530

    Solid (#12)
    * learned the first 3 principles of SOLID
    
    * Learned ISP
    
    * done with the hands-on for DIP
    
    * designed a document editor:
    
    * learned strategy pattern and factory design pattern

---
+++ Factory Design Pattern/AbstractFactoryMethod.cpp
@@ -0,0 +1,151 @@
+#include<iostream>
+using namespace std;
+
+class Burger {
+    public:
+        virtual void prepare() = 0;
+};
+
+class BasicBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Cooking Basic Burger with aloo tikki and some indian masala"<<endl;
+        }
+};
+
+class StandardBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Cooking Standard Burger with korean tikki and some korean masala"<<endl;
+        }
+};
+
+class PremiumBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Cooking Premium Burger with american tikki, paneer, onion, cucumber and some oregano, chilli flakes"<<endl;
+        }
+};
+
+class BasicWheatBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Cooking Basic Wheat Burger with aloo tikki and some indian masala"<<endl;
+        }
+};
+
+class StandardWheatBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Cooking Standard Wheat Burger with korean tikki and some korean masala"<<endl;
+        }
+};
+
+class PremiumWheatBurger : public Burger {
+    public:
+        void prepare() override {
+            cout<<"Cooking Premium Wheat Burger with american tikki, paneer, onion, cucumber and some oregano, chilli flakes"<<endl;
+        }
+};
+
+//Product 2
+class GarlicBread{
+    public:
+        virtual void prepare() = 0;
+};
+
+class BasicGarlicBread : public GarlicBread {
+    public:
+        void prepare() override {
+            cout<<"Cooking Basic Garlic bread using some secret ingrediants"<<endl;
+        }
+};
+
+class CheeseGarlicBread : public GarlicBread {
+    public:
+        void prepare() override {
+            cout<<"Cooking Cheese Garlic bread using some secret ingrediants"<<endl;
+        }
+};
+
+class BasicWheatGarlicBread : public GarlicBread {
+    public:
+        void prepare() override {
+            cout<<"Cooking Basic Wheat Garlic bread using some secret ingrediants"<<endl;
+        }
+};
+
+class CheeseWheatGarlicBread : public GarlicBread {
+    public:
+        void prepare() override {
+            cout<<"Cooking Cheese Garlic bread using some secret ingrediants and wheat"<<endl;
+        }
+};
+
+class BurgerFactory {
+    public:
+        virtual Burger* cookBurger(string type) = 0;
+        virtual GarlicBread* cookGarlicBread(string type) = 0;
+};
+
+class SinghBurger: public BurgerFactory {
+    Burger* cookBurger(string type) override {
+            if(type == "basic")
+                return new BasicBurger();
+            else if(type == "standard")
+                return new StandardBurger();
+            else if(type == "premium")
+                return new PremiumBurger();
+            else
+                cout<<"Invalid Burger type!"<<endl;
+                return nullptr;
+    }
+
+    GarlicBread* cookGarlicBread(string type) override {
+        if(type=="basic")
+            return new BasicGarlicBread;
+        else if(type=="cheese")
+            return new CheeseGarlicBread;
+        else
+            cout<<"Invalid type of garlic bread"<<endl;
+            return nullptr;
+    }
+
+};
+
+class KingBurger: public BurgerFactory {
+    Burger* cookBurger(string type) override {
+            if(type == "basic")
+                return new BasicWheatBurger();
+            else if(type == "standard")
+                return new StandardWheatBurger();
+            else if(type == "premium")
+                return new PremiumWheatBurger();
+            else
+                cout<<"Invalid Burger type!"<<endl;
+                return nullptr;
+    }
+
+    GarlicBread* cookGarlicBread(string type) override {
+        if(type=="basic")
+            return new BasicGarlicBread;
+        else if(type=="cheese")
+            return new CheeseGarlicBread;
+        else
+            cout<<"Invalid type of garlic bread"<<endl;
+            return nullptr;
+    }
+
+};
+
+int main(){
+    string BurgerType = "premium";
+    string GarlicBreadType = "cheese";
+    BurgerFactory* myfactory = new KingBurger();
+
+    Burger* myBurger = myfactory->cookBurger(BurgerType);
+    GarlicBread* myGarlicBread = myfactory->cookGarlicBread(GarlicBreadType);
+    myBurger->prepare();
+    myGarlicBread->prepare();
+return 0;
+}
